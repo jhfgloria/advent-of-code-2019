cad09360aec794d6b3c383c85693787fdf528e83
Joao Gloria <jhfgloria@gmail.com>
2019-12-04 21:08:25 +0000

    Day 2 Part 2

--- day-2/program.exs
+++ day-2/program.exs
@@ -9,14 +9,29 @@ defmodule DayTwo do
     end
   end
 end
-
 {:ok, body} = File.read("input.txt")
 
 codes =
   body
   |> String.split(",", trim: true)
   |> Enum.map(fn x -> String.to_integer(x) end)
-  |> List.update_at(1, &(&1 - &1 + 12))
-  |> List.update_at(2, &(&1 - &1 + 2))
+  # |> List.update_at(1, &(&1 - &1 + 12))
+  # |> List.update_at(2, &(&1 - &1 + 2))
 
-IO.puts(DayTwo.intcode(codes, 0))
+# IO.puts(DayTwo.intcode(codes, 0))
+
+complex = fn({n, v}, e) ->
+  codes
+  |> List.update_at(1, fn _ -> n end)
+  |> List.update_at(2, fn _ -> v end)
+  |> DayTwo.intcode(0)
+  |> (fn x -> if x == e do {n,v} else nil end end).()
+end
+
+ 0..99
+ |> Enum.flat_map(fn x -> Enum.zip(0..99, Enum.map(0..99 , fn _ -> x end)) end)
+ |> Enum.map(&(complex.(&1, 19690720)))
+ |> Enum.filter(&(!is_nil(&1)))
+ |> IO.inspect
+
+
